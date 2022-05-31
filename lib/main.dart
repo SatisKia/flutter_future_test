930629c6d28c127e3b9a1b969215e98d1d341d79
SatisKia <satis@msd.biglobe.ne.jp>
2022-05-31 16:39:58 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyHomePageState extends State {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       test1( this );
 //      test2( this );
 //      test3( this );
